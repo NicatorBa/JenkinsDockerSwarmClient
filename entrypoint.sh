2cd46cb5533701f0307a035b321ec7e125e04b39
NicatorBa <daniel@bartschinski.info>
2017-10-26 22:43:36 +0200

    predefine optional parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+EXECUTORS=${EXECUTORS:-1}
+NAME=${NAME:-client-$(hostname)}
+
 mkdir -p /var/jenkins
 java -jar swarm-client.jar -fsroot /var/jenkins -executors ${EXECUTORS} -labels ${LABELS} -name ${NAME} -master ${MASTER} -username ${USER_NAME} -password ${USER_PASSWORD}
