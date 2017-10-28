97c616eb13a120493226c94c4d7909848d85182c
NicatorBa <daniel@bartschinski.info>
2017-10-28 13:39:23 +0200

    Fix NAME to use on multiple servers

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [[ -z "${USER_PASSWORD}" ]]; then
 fi
 
 EXECUTORS=${EXECUTORS:-1}
-NAME=${NAME:-client-$(hostname)}
+NAME=${NAME:-client}
 
 mkdir -p /var/jenkins
-java -jar swarm-client.jar -fsroot /var/jenkins -executors ${EXECUTORS} -labels ${LABELS} -name ${NAME} -master ${MASTER} -username ${USER_NAME} -password ${USER_PASSWORD}
+java -jar swarm-client.jar -fsroot /var/jenkins -executors ${EXECUTORS} -labels ${LABELS} -name ${NAME}-$(hostname) -master ${MASTER} -username ${USER_NAME} -password ${USER_PASSWORD}
